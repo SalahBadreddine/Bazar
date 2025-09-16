72467b544309c3c13407074e2ea894f2e250d81c
SalahBadreddine <salah.badreddine@ensia.edu.dz>
2025-09-16 21:37:19 +0100

    defined colors and added splash screen

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const primary50 = Color(0xFFFAF9FD);
+  static const primary100 = Color(0xFFE5DEF8);
+  static const primary200 = Color(0xFFCABCEF);
+  static const primary300 = Color(0xFFA28CE0);
+  static const primary400 = Color(0xFF7D64C3);
+  static const primary500 = Color(0xFF54408C);
+  static const primary600 = Color(0xFF352368);
+  static const primary700 = Color(0xFF251554);
+  static const primary800 = Color(0xFF10052F);
+  static const primary900 = Color(0xFF09031B);
+
+  // Grey scale
+  static const greyscale50 = Color(0xFFFAFAFA);
+  static const greyscale100 = Color(0xFFF5F5F5);
+  static const greyscale200 = Color(0xFFE8E8E8);
+  static const greyscale300 = Color(0xFFD6D6D6);
+  static const greyscale400 = Color(0xFFB8B8B8);
+  static const greyscale500 = Color(0xFFA6A6A6);
+  static const greyscale600 = Color(0xFF7A7A7A);
+  static const greyscale700 = Color(0xFF454545);
+  static const greyscale800 = Color(0xFF292929);
+  static const greyscale900 = Color(0xFF121212);
+
+  // Additional colors
+  static const white = Color(0xFFFFFFFF);
+  static const yellow = Color(0xFFFBAE05);
+  static const orange = Color(0xFFFF8C39);
+  static const red = Color(0xFFEF5A56);
+  static const blue = Color(0xFF3784FB);
+}
